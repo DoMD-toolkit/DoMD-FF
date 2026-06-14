134df4849d992cb24960e29eda6156d13fce1a6e
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2026-06-14 12:59:40 +0800

    FIX DIHEDRAL SEARCHING BUG

--- test/test.py
+++ test/test.py
@@ -8,22 +8,12 @@ if __name__ == '__main__':
     from openbabel import openbabel as ob
     import logging
     from misc.logger import logger
+    from misc.parser import molecule_reader
     logger.setLevel(logging.DEBUG)
     logger.propagate = True
 
-    #rdmol = Chem.MolFromPDBFile('test_data/test_system.sdf', removeHs=False)
-    suppl = Chem.SDMolSupplier('test_data/test_system.sdf', removeHs=False)
-    rdmol = None
-    for mol in suppl:
-        if mol is None: continue
-        if rdmol is None:
-            rdmol = mol
-        else:
-            rdmol = Chem.CombineMols(rdmol, mol)
-    obmol = ob.OBMol()
-    conv = ob.OBConversion()
-    conv.SetInFormat('sdf')
-    conv.ReadFile(obmol, 'test_data/test_system.sdf')
+    # obmol, rdmol, coordinates, res_names, res_ids, box_tensor = molecule_reader('test_data/test_system.pdb')
+    obmol, rdmol, coordinates, res_names, res_ids, box_tensor = molecule_reader('test_data/test_system.sdf')
     forcefield = FF('opls')
     forcefield.setup(rdmol, obmol, useGMX=True, useBOSS=True)
 
