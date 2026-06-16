05fe480a84ba95c873db5ab6fd45b2a4369aad7e
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2026-06-16 21:33:11 +0800

    FIX DIHEDRAL SEARCHING BUG

--- server.py
+++ server.py
@@ -173,4 +173,4 @@ async def download_result(task_id: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
