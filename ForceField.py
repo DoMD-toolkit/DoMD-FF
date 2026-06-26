64c7c3f4aaf29d94db4d672e65ca880d2ad1906e
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2026-06-26 16:17:18 +0800

    FIX DIHEDRAL SEARCHING BUG

--- ForceField.py
+++ ForceField.py
@@ -18,13 +18,12 @@ class FF(object):
         self._missing = None
         self.charges = {}
 
-    def setup(self, rdmol: Chem.Mol, obmol: ob.OBMol = None, **kwargs):
+    def setup(self, rdmol: Chem.Mol, obmol: ob.OBMol = None, charge_factor: float = 1, **kwargs):
         self.rdmol = rdmol
         self.obmol = obmol
         formal_charge = Chem.GetFormalCharge(rdmol)
         logger.info(f"Formal charge of molecule {rdmol}: {formal_charge:.4f}")
         charge_drift = atom_count = 0
-
 
         if self.name == 'opls':
             self.params, self._missing, self.success = opls_setup(rdmol, obmol, **kwargs)
@@ -35,8 +34,8 @@ class FF(object):
                 logger.info(f"OPLS total charge for {rdmol}: {charge_drift:.4f}")
                 charge_drift /= atom_count
                 for idx in self.params[0]:
-                    self.charges[idx] = self.params[0][idx].charge - charge_drift + formal_charge / atom_count
-                logger.info(f"OPLS reset total charge to formal charge {formal_charge:.4f}.")
+                    self.charges[idx] = (self.params[0][idx].charge - charge_drift + formal_charge / atom_count) * charge_factor
+                logger.info(f"OPLS reset total charge to formal charge {formal_charge * charge_factor:.4f}.")
 
     # TODO: add MD modules
     def energy(self):
