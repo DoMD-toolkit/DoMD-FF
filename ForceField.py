1344d62bfbf95232a324902ca92bc30c28aa1e5c
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2026-07-04 18:48:24 +0800

    Update ForceField.py

--- ForceField.py
+++ ForceField.py
@@ -24,20 +24,64 @@ class FF(object):
         self.obmol = obmol
         formal_charge = Chem.GetFormalCharge(rdmol)
         logger.debug(f"Formal charge of molecule {rdmol}: {formal_charge:.4f}")
-        charge_drift = atom_count = 0
 
         if self.name == 'opls':
             self.params, self._missing, self.success, self._meta = opls_setup(rdmol, obmol, **kwargs)
             if self.success:
+                ion_indices = []
+                non_ion_indices = []
+                for atom in rdmol.GetAtoms():
+                    idx = atom.GetIdx()
+                    if atom.GetDegree() == 0:
+                        ion_indices.append(idx)
+                    else:
+                        non_ion_indices.append(idx)
+                atom_count = len(self.params[0])
+
+                total_opls_charge = sum(float(self.params[0][idx].charge) for idx in self.params[0])
+                logger.debug(f"OPLS raw total charge for {rdmol}: {total_opls_charge:.4f}")
+
+                global_drift_per_atom = total_opls_charge / atom_count
+                temp_charges = {}
                 for idx in self.params[0]:
-                    charge_drift += float(self.params[0][idx].charge)
-                    atom_count += 1
-                logger.debug(f"OPLS total charge for {rdmol}: {charge_drift:.4f}")
-                charge_drift /= atom_count
-                for idx in self.params[0]:
-                    self.charges[idx] = (self.params[0][
-                                             idx].charge - charge_drift + formal_charge / atom_count) * charge_factor
-                logger.debug(f"OPLS reset total charge to formal charge {formal_charge * charge_factor:.4f}.")
+                    temp_charges[idx] = self.params[0][idx].charge - global_drift_per_atom + (
+                                formal_charge / atom_count)
+
+                need_ion_constraint = False
+                for idx in ion_indices:
+                    atom_formal_charge = rdmol.GetAtomWithIdx(idx).GetFormalCharge()
+                    if temp_charges[idx] > atom_formal_charge:
+                        need_ion_constraint = True
+                        logger.debug(f"Ion constraint triggered by atom {idx} "
+                                     f"(Calculated: {temp_charges[idx]:.4f} > Formal: {atom_formal_charge})")
+                        break
+
+
+                if not need_ion_constraint:
+                    for idx in self.params[0]:
+                        self.charges[idx] = temp_charges[idx] * charge_factor
+                    logger.info(f"OPLS reset total charge to formal charge "
+                                 f"{formal_charge * charge_factor:.4f} (Global uniform distribution).")
+
+                else:
+                    ion_total_charge = 0.0
+                    for idx in ion_indices:
+                        atom_fc = float(rdmol.GetAtomWithIdx(idx).GetFormalCharge())
+                        self.charges[idx] = atom_fc * charge_factor
+                        ion_total_charge += atom_fc
+
+                    if non_ion_indices:
+                        target_non_ion_charge = formal_charge - ion_total_charge
+
+                        non_ion_opls_charge = sum(float(self.params[0][idx].charge) for idx in non_ion_indices)
+                        non_ion_count = len(non_ion_indices)
+                        non_ion_drift_per_atom = non_ion_opls_charge / non_ion_count
+
+                        for idx in non_ion_indices:
+                            self.charges[idx] = (self.params[0][idx].charge - non_ion_drift_per_atom + (
+                                        target_non_ion_charge / non_ion_count)) * charge_factor
+                    logger.info(f"OPLS reset total charge to formal charge {formal_charge * charge_factor:.4f} "
+                                f"(Ion constrained, {len(non_ion_indices)} non-ions adjusted).")
 
     # TODO: add MD modules
     def energy(self):
